f717532c5dca4e49c607b2c4f1221e4933f3ee01
svenkaestle <sven.kaestle@gmx.de>
2017-06-17 23:48:38 +0200

    New MainActivity layout

--- app/src/main/java/de/svenkaestle/prapp/MainActivity.java
+++ app/src/main/java/de/svenkaestle/prapp/MainActivity.java
@@ -1,25 +1,67 @@
 package de.svenkaestle.prapp;
 
-import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.imanoweb.calendarview.CalendarListener;
+import com.imanoweb.calendarview.CustomCalendarView;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int STOCK = 10;
+    public static final Boolean safety = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        initializeSafety();
+        initializeCustomCalendar();
+        initializeStock();
+
+    } // end onCreate
+
+    private void initializeSafety() {
+        TextView textViewSafety = (TextView) findViewById(R.id.safety);
+        if (safety) {
+            textViewSafety.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.safety_green));
+        } else {
+            textViewSafety.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.safety_red));
+        }
+    } // end initializeSafety
+
+    private void initializeCustomCalendar() {
+        CustomCalendarView calendarView = (CustomCalendarView) findViewById(R.id.calendarView);
+        calendarView.setCalendarListener(new CalendarListener() {
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), "Date: " + dayOfMonth + "." + month + "." + year, Toast.LENGTH_SHORT).show();
+            public void onDateSelected(Date date) {
+                Toast.makeText(getApplicationContext(), "Date: " + date, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onMonthChanged(Date date) {
             }
         });
 
-    }
-}
+    } // end initializeCustomCalendar
+
+    private void initializeStock() {
+        TextView textViewStock = (TextView) findViewById(R.id.stock);
+        textViewStock.setText("Stock: " + STOCK);
+        if (STOCK > 5) {
+            textViewStock.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+        } else {
+            textViewStock.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.safety_red));
+        }
+    } // end initializeStock
+
+
+
+} // end class MainActivity
