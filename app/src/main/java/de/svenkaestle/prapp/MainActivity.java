2996911cbfe775c2947e92ee947a35ad6cd316f4
svenkaestle <sven.kaestle@gmx.de>
2017-06-17 21:38:39 +0200

    Extract date from calendar

--- app/src/main/java/de/svenkaestle/prapp/MainActivity.java
+++ app/src/main/java/de/svenkaestle/prapp/MainActivity.java
@@ -1,7 +1,10 @@
 package de.svenkaestle.prapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), "Date: " + dayOfMonth + "." + month + "." + year, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
