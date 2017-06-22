3c7981392252b4e73a9c76cb78d7b54c979e260d
svenkaestle <sven.kaestle@gmx.de>
2017-06-22 10:14:47 +0200

    New PlanObject Class

---
+++ app/src/main/java/de/svenkaestle/prapp/ObjectClasses/PlanObject.java
@@ -0,0 +1,72 @@
+package de.svenkaestle.prapp.ObjectClasses;
+
+/**
+ * Created by ivan on 22.06.17.
+ */
+
+public class PlanObject {
+
+    /* variables */
+    private int id;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String timestamp;
+
+    public PlanObject(int id, String startDate, String endDate, String description, String timestamp) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.timestamp = timestamp;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + Integer.toString(id) +
+                " | startDate: " + startDate +
+                " | endDate: " + endDate +
+                " | description: " + description +
+                " | timestamp: " + timestamp;
+    }
+}
