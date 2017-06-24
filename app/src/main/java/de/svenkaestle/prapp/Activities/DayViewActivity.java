fbcce51eff778d3d2e43a9af46c84fd265999cee
magratheaner <magratheaner@gmail.com>
2017-06-24 17:22:56 +0200

    Add recycle view to activity code and day view entry adapter

--- app/src/main/java/de/svenkaestle/prapp/Activities/DayViewActivity.java
+++ app/src/main/java/de/svenkaestle/prapp/Activities/DayViewActivity.java
@@ -3,17 +3,39 @@ package de.svenkaestle.prapp.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.svenkaestle.prapp.Helper.DayViewEntryAdapter;
 import de.svenkaestle.prapp.R;
 
 public class DayViewActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_view);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.day_view_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        // mAdapter = new DayViewEntryAdapter(myDataset);
+        // mRecyclerView.setAdapter(mAdapter);
+
     }
 
     @Override
