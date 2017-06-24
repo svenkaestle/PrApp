4dcce4bff11e8e1f8cfe7881a3e3965882909fa0
magratheaner <magratheaner@gmail.com>
2017-06-24 15:53:06 +0200

    Create day view activity as empty activity

---
+++ app/src/main/java/de/svenkaestle/prapp/Activities/DayViewActivity.java
@@ -0,0 +1,15 @@
+package de.svenkaestle.prapp.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.svenkaestle.prapp.R;
+
+public class DayViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day_view);
+    }
+}
