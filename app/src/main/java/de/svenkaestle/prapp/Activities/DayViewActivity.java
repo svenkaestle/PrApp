56efca67e92f58aa1f9219dd55f626ef34025ed8
magratheaner <magratheaner@gmail.com>
2017-06-24 16:24:52 +0200

    Change day view title and add options menu boilerplate

--- app/src/main/java/de/svenkaestle/prapp/Activities/DayViewActivity.java
+++ app/src/main/java/de/svenkaestle/prapp/Activities/DayViewActivity.java
@@ -1,7 +1,10 @@
 package de.svenkaestle.prapp.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import de.svenkaestle.prapp.R;
 
@@ -11,5 +14,26 @@ public class DayViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_day_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_links:
+                startActivity(new Intent(getApplicationContext(), LinksActivity.class));
+                return true;
+            case R.id.menu_contact:
+                startActivity(new Intent(getApplicationContext(), ContactActivity.class));
+                return true;
+            default:
+                finish();
+                return true;
+        }
     }
 }
